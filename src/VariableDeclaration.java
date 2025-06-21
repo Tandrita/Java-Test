97276aa467a3d1c6e385e808ce414137abbc3158
Tandrita Dewanjee <tandritadewanjee63@gmail.com>
2025-06-21 11:39:38 +0530

    21 Jun 2025

---
+++ src/VariableDeclaration.java
@@ -0,0 +1,18 @@
+public class VariableDeclaration {
+public static void main(String[] args) {
+
+    // Declare and initialize primitive data types
+    int age = 25;
+    double accountBalance = 10500.75;
+    float heightInMeters = 1.65f;
+    double temperatureCelsius = 36.6;
+    char grade = 'A';
+
+    // Print the values
+    System.out.println("Age: " + age);
+    System.out.println("Account Balance: " + accountBalance);
+    System.out.println("heightInMeters: " + heightInMeters);
+    System.out.println("Grade: " + grade);
+    System.out.println("Temperature (Celsius): " + temperatureCelsius);
+}
+}
